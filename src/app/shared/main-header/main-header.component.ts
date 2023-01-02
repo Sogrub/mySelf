5ee2c5236f9628cf9e60afbffa99b70b092df957
dburgos <dburgos@econtainers.co>
2023-01-02 18:51:35 -0500

    Inicio Header con ScrollBar

--- src/app/shared/main-header/main-header.component.ts
+++ src/app/shared/main-header/main-header.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main-header',
   templateUrl: './main-header.component.html',
   styleUrls: ['./main-header.component.scss']
 })
-export class MainHeaderComponent implements OnInit {
+export class MainHeaderComponent implements OnInit, AfterViewInit {
+  @ViewChild('main') main!: ElementRef;
+  @ViewChild('header') header!: ElementRef;
 
   toggleActive: boolean = false;
+  menuActive: boolean = false;
 
-  constructor() { }
+  @HostListener('window:resize')
+  onResize() {
+    this.main.nativeElement.style.height = window.innerHeight + 'px';
+  }
+
+  @HostListener("window:scroll", ['$event'])
+  onScroll(){
+    this.header.nativeElement.classList.toggle('sticky', window.scrollY > 0);
+    if (window.scrollY > 0) {
+      this.menuActive = true;
+    } else {
+      this.menuActive = false;
+    }
+  }
+
+  constructor() { 
+    
+  }
+  ngAfterViewInit(): void {
+    this.main.nativeElement.style.height = window.innerHeight + 'px';
+  }
 
   ngOnInit(): void {
   }
